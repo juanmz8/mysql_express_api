5b50705e7fae886dfb695349bfaf635569a924b5
juan miguez <juanmiguezar@gmail.com>
2024-01-14 00:07:18 -0300

    Corrigiendo errores en las rutas

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import employeesRoutes from "./routes/employes.routes.js";
 const app = express();
 
 app.use(express.json())
-app.use("/api",employeesRoutes);
+app.use("https://mysqlexpressapi-production.up.railway.app/api",employeesRoutes);
 
 app.use((req, res,next) => {
    res.status(404).json({
